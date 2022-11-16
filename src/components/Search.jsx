a7d31f17c222b6c214a76638470b13e3da57e89b
B4n3l1ng <114490314+B4n3l1ng@users.noreply.github.com>
2022-11-16 16:27:33 +0000

    search functionality almost

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,23 +4,18 @@ import { useState } from 'react';
 // Iteration 5
 function Search({foodArray,setFoodArray}) {
     const [nameSearch, setNameSearch] = useState("")
-    const handleSearch = event => {
+    const handleSearch = (event) => {
+        setNameSearch(event.target.value.toLowerCase())
         event.preventDefault()
-        if (nameSearch.length===0){
-            setFoodArray(foodArray)
-        } else {
-            const searchArray = [...foodArray]
-            setFoodArray(searchArray.filter(element => element.name.toLowerCase().includes(nameSearch)))
-            
-        }
-        setNameSearch("")
+        const searchArray = [...foodArray]
+        setFoodArray(searchArray.filter(element => element.name.toLowerCase().includes(nameSearch)))
     }
   return (
     <>
       <Divider>Search</Divider>
 
       <label>Search</label>
-      <Input value={nameSearch} type="text" onChange={(event) => setNameSearch((event.target.value).toLowerCase())} onInput={handleSearch}/>
+      <Input value={nameSearch} type="text" onChange={(event) => handleSearch(event)} />
     </>
   );
 }
